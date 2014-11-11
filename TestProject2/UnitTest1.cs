f3771b754a43a02d8a542b3f8412f36b35c027e3
CristinaGlz <cgm00001@gmail.com>
2014-11-11 01:36:28 -0600

    c#
    proyecto en c#

---
+++ TestProject2/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿
+using NUnit.Framework;
+
+namespace TestProject2
+{
+    [TestFixture()]
+    public class TestProject2
+    {
+        [Test()]
+        public void TestMethod1()
+        {
+            Calculadora cal = new Calculadora();
+            Assert.AreEqual(4, cal.suma(2, 2));
+        }
+    }
+}
